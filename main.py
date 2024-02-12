86bbc929939bbb3de4a8c1f6935f6500138e1745
bthee <b.theeck08@gmail.com>
2024-02-12 12:39:11 +0100

    added labels to window

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ class Window:
         self.root.title("Password Strength Test")
         self.root.geometry(f"{width}x{heigth}")
         self.root.configure(bg="white")
+
+        self.welcome = Label(self.root,
+                             text="Welcome!",
+                             font=("", 16),
+                             bg="lightblue"
+                            )
+        
+        self.instruction = Label(self.root,
+                                 text="To test your password, just enter it in the field below and press 'enter' or hit the test button.",
+                                 font=("", 12),
+                                 padx=10,
+                                 anchor="w",
+                                 bg="white"
+                                )
+        
+        self.welcome.place(x=0, y=0, width=800, height=50)
+        self.instruction.place(x=0, y=80, width=800, height=50)
