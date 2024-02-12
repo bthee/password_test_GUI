9b8ce7644bf742eab2a398f2f2562c80382581e2
bthee <b.theeck08@gmail.com>
2024-02-12 13:24:12 +0100

    started to add logic for password check

--- main.py
+++ main.py
@@ -28,28 +28,50 @@ class Window:
         self.output = Label(self.root,
                             font=("", 12),
                             padx=10,
-                            anchor="w",
+                            pady=10,
+                            anchor="nw",
                             bg="lightgray"
                             )
         
         self.test_button = Button(self.root,
-                                  text="Check your password!"
+                                  text="Check your password!",
+                                  command=self.display_output
                                   )
 
 
         self.welcome.place(x=0, y=0, width=800, height=50)
         self.instruction.place(x=0, y=80, width=800, height=50)
         self.user_input.place(x=200, y=200, width=400, height=50)
-        self.output.place(x=100, y=280, width=600, height=200)
+        self.output.place(x=50, y=280, width=700, height=200)
         self.test_button.place(x=300, y=500, width=200, height=50)
 
         self.root.mainloop()
 
 
-    def get_input(self):
-         self.output.config(text= self.user_input.get(), 
-                            font= ('Helvetica 13')
-                            )
+    def display_output(self):
+        input_text = self.user_input.get()
+        length = len(input_text) >= 12
+        number = any(char.isnumeric() for char in input_text)
+        upper_ch = any(char.isupper() for char in input_text)
+        lower_ch = any(char.islower() for char in input_text)
+        special_ch = any(not char.isalnum() for char in input_text)
+
+        count = sum([length, number, upper_ch, lower_ch, special_ch])
+
+        if count == 5:
+            output_text = "Your password is strong!"
+        elif count == 4 and length:
+            output_text = "Your password is good! Here is an improvement you should make."
+        elif count == 4 and not length:
+            output_text = "Your password would be strong if you increase its length."
+        elif count == 3 and length:
+            output_text = "Your password is okay. Here are some improvements you should make."
+        elif count == 3 and not length:
+            output_text = "Your password would be strong if you increase its length."
+        elif count <= 2:
+            output_text = "Your password is very weak! You should make the following improvements!"
+        
+        self.output.config(text=output_text)
 
 
 def main():
